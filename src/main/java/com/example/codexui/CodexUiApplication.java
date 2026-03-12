e581618717df124bf3cc5158332cb733d56e4cfa
gaoziteng <1379129015@qq.com>
2026-03-12 20:23:03 +0800

    Initial commit

---
+++ src/main/java/com/example/codexui/CodexUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.codexui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codexui/CodexUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codexui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
